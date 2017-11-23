2efd4667f261ea200de90c0f4a38b39588bd765b
marcello96 <jakubowskimarcin96@gmail.com>
2017-11-23 14:54:32 +0100

    Hello world test

---
+++ socialForce/src/Pedestrian.java
@@ -0,0 +1,8 @@
+public class Pedestrian {
+    private double[] velocity;
+    private double[] xVec;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
