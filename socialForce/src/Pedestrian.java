271d201217aa76d10a3e354768bb919a84ef2c0a
marcello96 <jakubowskimarcin96@gmail.com>
2017-11-25 16:01:00 +0100

    Project language is changed to C++.

--- socialForce/src/Pedestrian.java
+++
@@ -1,8 +0,0 @@
-public class Pedestrian {
-    private double[] velocity;
-    private double[] xVec;
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
