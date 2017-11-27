2a2cc8f96c0dbb37addb5334a36a4370792edaec
marcello96 <jakubowskimarcin96@gmail.com>
2017-11-27 22:39:05 +0100

    next part

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "pedestrian.h"
+#include "agent.h"
 
 using namespace std;
 
 int main()
 {
-    Pedestrian p1(0,0,1.4,-2.3);
+    Agent p1(0,0,1.4,-2.3);
     cout << "Hello world!" << endl;
     cout << "Im new pedestian..." << endl;
     cout << "My position is (" << p1.getPositionX() << "," << p1.getPositionY() << ")" << endl;
